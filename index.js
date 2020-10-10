3af850cbd8ab3efa6cf31d3c1f6218f12444af7e
brauermi <brauemi@student.uzlabina.cz>
2020-10-10 14:23:13 +0200

    - ahoj command

--- index.js
+++ index.js
@@ -71,10 +71,6 @@ bot.on("message", async message => {
 
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if (commandfile) commandfile.run(bot, message, args);
-
-    if (cmd === `ahoj`) {
-        return message.channel.send("Hello my friendo!");
-    }
 });
 
 var quotes = quoteconfig.quotes;
